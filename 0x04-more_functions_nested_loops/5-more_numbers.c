ef0b40f2e85e231ef483891d2b3eb752accf8466
Harris Chatuwa <harrischatuwa@gmail.com>
2023-09-14 14:06:59 -0700

    update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,18 @@
  */
 void more_numbers(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 10; i++) /* loop 10 times */
-    {
-        for (j = 0; j <= 14; j++) /* loop from 0 to 14 */
-        {
-            if (j > 9) /* if j is a two-digit number */
-            {
-                _putchar((j / 10) + '0'); /* print the first digit */
-            }
-            _putchar((j % 10) + '0'); /* print the second digit or the single digit */
-        }
-        _putchar('\n'); /* print a new line */
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+				_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
