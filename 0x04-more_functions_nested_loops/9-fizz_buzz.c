a5dd920180e955f1d8bfec8fdb4bf59c343ec19f
Harris Chatuwa <harrischatuwa@gmail.com>
2023-09-14 16:40:43 -0700

    fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
+/**
+ * main - programs enrty point
+ * Return: Alsways 0 (sucess)
+ */
 
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        int fizz = i % 3 == 0;
-        int buzz = i % 5 == 0;
+	for (i = 1; i <= 100; i++)
+	{
+		int fizz = i % 3 == 0;
+		int buzz = i % 5 == 0;
 
-        if (fizz)
-            printf("Fizz");
+		if (fizz)
+			printf("Fizz");
 
-        if (buzz)
-            printf("Buzz");
+		if (buzz)
+			printf("Buzz");
 
-        if (!fizz && !buzz)
-            printf("%d", i);
+		if (!fizz && !buzz)
+			printf("%d", i);
 
-        if (i < 100)
-            printf(" ");
-        else
-            printf("\n");
-    }
+		if (i < 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
 
-    return 0;
+	return 0;
 }
