a3a78436e65fae6cd7ba5a01d2bc8cd2f043205f
Harris Chatuwa <harrischatuwa@gmail.com>
2023-09-14 10:09:24 -0700

    alx 2-mul multiplier function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Multiplies two numbers and returns the result
+ * @a: multiplier1
+ * @b: multiplier2
+ * Return: result of the math
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
