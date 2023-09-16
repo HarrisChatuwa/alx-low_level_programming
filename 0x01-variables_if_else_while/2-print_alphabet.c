c4f939cdcf1826f84c8e281778d346c91835334b
Harris Chatuwa <harrischatuwa@gmail.com>
2023-09-16 00:57:55 -0700

    prints the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Return: Alaywas 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
