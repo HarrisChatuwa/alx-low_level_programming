3574cb0e1f79b33ec6cdd352ae160788d04cbc1e
Harris Chatuwa <harrischatuwa@gmail.com>
2023-09-17 14:29:18 -0700

    prins from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_to_98 -  prints all natural numbers from n to 98
+ * @n: first number to be printed
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i < 99; i++)
+		_putchar(i);
+}
