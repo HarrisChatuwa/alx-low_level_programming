b96a3f08b73a9a6703a239a682df0bfde0e1c9db
Harris Chatuwa <harrischatuwa@gmail.com>
2023-09-18 14:09:10 -0700

    printes string in rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string to print in rev
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (s[len] != '\0')
+		len++;
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
