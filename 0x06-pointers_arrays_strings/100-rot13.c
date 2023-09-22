5253c6acd6a93e19ad56e736385546e7e922e38c
Harris Chatuwa <harrischatuwa@gmail.com>
2023-09-22 03:29:55 -0700

    updated

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,38 +1,23 @@
 #include "main.h"
 
 /**
- * rot13 - Encodes a string using ROT13.
- * @str: The input string.
- *
- * Return: A pointer to the modified string.
+ * rot13 - encodes
+ * @str: parameter
+ * Return: str
  */
 char *rot13(char *str)
 {
 	char *ptr = str;
-	char *alpha_lower = "abcdefghijklmnopqrstuvwxyz";
-	char *alpha_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char *rot13_lower = "nopqrstuvwxyzabcdefghijklm";
-	char *rot13_upper = "NOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	while (*ptr)
 	{
-		int i = 0;
+		char offset = (*ptr >= 'a' && *ptr <= 'z') ? 'a' : 'A';
 
 		if ((*ptr >= 'a' && *ptr <= 'z') || (*ptr >= 'A' && *ptr <= 'Z'))
 		{
-			char *alpha_set = (*ptr >= 'a' && *ptr <= 'z') ? alpha_lower : alpha_upper;
-			char *rot13_set = (*ptr >= 'a' && *ptr <= 'z') ? rot13_lower : rot13_upper;
-
-			while (alpha_set[i])
-			{
-				if (*ptr == alpha_set[i])
-				{
-					*ptr = rot13_set[i];
-					break;
-				}
-				i++;
-			}
+			*ptr = ((*ptr - offset + 13) % 26) + offset;
 		}
+
 		ptr++;
 	}
 
