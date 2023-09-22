ff6422b594e4797801f90fa30af6fe3f3e1228aa
Harris Chatuwa <harrischatuwa@gmail.com>
2023-09-21 23:39:39 -0700

    updated 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,24 +8,25 @@
  */
 char *leet(char *str)
 {
-    char *ptr = str;
-    char *leet_chars = "aAeEoOtTlL";
-    char *leet_replacements = "4433007711";
+	char *ptr = str;
+	char *leet_chars = "aAeEoOtTlL";
+	char *leet_replacements = "4433007711";
 
-    while (*ptr)
-    {
-        int i = 0;
-        while (leet_chars[i])
-        {
-            if (*ptr == leet_chars[i])
-            {
-                *ptr = leet_replacements[i];
-                break;
-            }
-            i++;
-        }
-        ptr++;
-    }
+	while (*ptr)
+	{
+		int i = 0;
 
-    return str;
+		while (leet_chars[i])
+		{
+			if (*ptr == leet_chars[i])
+			{
+				*ptr = leet_replacements[i];
+				break;
+			}
+			i++;
+		}
+		ptr++;
+	}
+
+	return (str);
 }
